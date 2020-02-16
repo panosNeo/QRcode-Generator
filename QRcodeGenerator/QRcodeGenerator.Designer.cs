c45b73a966273dc270cd429f88482ea44a218872
Panos Neo <44003014+panosNeo@users.noreply.github.com>
2020-02-16 02:30:42 +0200

    Upload files

---
+++ QRcodeGenerator/QRcodeGenerator.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace QRcodeGenerator
+{
+    partial class QRcodeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.crtimg = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 307);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Location = new System.Drawing.Point(104, 369);
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(232, 20);
+            this.txtEncode.TabIndex = 1;
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Location = new System.Drawing.Point(104, 420);
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(232, 20);
+            this.txtDecode.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(149, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // crtimg
+            // 
+            this.crtimg.Location = new System.Drawing.Point(342, 366);
+            this.crtimg.Name = "crtimg";
+            this.crtimg.Size = new System.Drawing.Size(75, 23);
+            this.crtimg.TabIndex = 3;
+            this.crtimg.Text = "Create";
+            this.crtimg.UseVisualStyleBackColor = true;
+            this.crtimg.Click += new System.EventHandler(this.crtimg_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(342, 417);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 4;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Add something here to create a qrcode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Open an existing qrcode";
+            // 
+            // QRcodeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(522, 520);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.crtimg);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.txtEncode);
+            this.Controls.Add(this.panel1);
+            this.Name = "QRcodeGenerator";
+            this.Text = "QRcode Generator";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.Button crtimg;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QRcodeGenerator/QRcodeGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MessagingToolkit.QRCode.Codec.Data;
+using System.Drawing.Imaging;
+
+namespace QRcodeGenerator
+{
+    public partial class QRcodeGenerator : Form
+    {
+        public QRcodeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void crtimg_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+                    encoder.QRCodeScale = 8;
+                    Bitmap bmp = encoder.Encode(txtEncode.Text);
+                    pictureBox1.Image = bmp;
+                    bmp.Save(sfd.FileName, ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(ofd.FileName);
+                    MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+                    txtDecode.Text = decoder.Decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap));
+                }
+            }
+        }
+    }
+}
